eed0959029bdce20096efd2fdedc093b207b5fd4
In-tillel <t50_t.j.jenishbekov@mpt.ru>
2023-02-06 10:55:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Ежедневник;
+
+namespace SimpleNotebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file_name = "SimpleNotebook.txt";
+            string state = null;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
+            Console.WriteLine("Запускаем простой ежедневник . .");
+            if (System.IO.File.Exists(Convert.ToString(Path.GetFullPath(file_name))) == false)
+                Console.WriteLine("Не найден файл SimpleNotebook.txt. . он будет создан автоматически . .");
+            Console.WriteLine("Готово . .");
+            while (state != "3")
+            {
+                try
+                {
+                    Console.WriteLine(" 1 - добавить новую запись  2 - найти запись  3 - выход");
+                    state = Console.ReadLine();
+                    switch (state)
+                    {
+                        case "1":
+                            pop temp = new pop();
+                            Console.WriteLine("Введите день недели: ");
+                            temp.Name = Console.ReadLine();
+                            Console.WriteLine("Что нужно сделать: ");
+                            temp.Wh = Console.ReadLine();
+                            Console.WriteLine("Введите время события: ");
+                            temp.time = Console.ReadLine();
+                            Console.WriteLine("Введите дату события: ");
+                            temp.BirthDate = Console.ReadLine();
+                            temp.WriteEmployee();
+                            break;
+                        case "2":
+                            string n = null;
+                            Console.WriteLine("Введите день недели который нужно вывести: ");
+                            n = Console.ReadLine();
+                            pop.DisplayEmployee(n);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("Работа завершена . . Нажмите клавишу для выхода . .");
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ pop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ежедневник
+{
+    internal class pop
+    {
+        public string Name { get; set; }
+        public string Wh { get; set; }
+        public string time { get; set; }
+        public string BirthDate { get; set; }
+        public pop() { }
+        public void WriteEmployee()
+        {
+            using (StreamWriter sw = File.AppendText("SimpleNotebook.txt"))
+            {
+                sw.WriteLine(this.Name);
+                sw.WriteLine(this.Wh);
+                sw.WriteLine(this.time);
+                sw.WriteLine(this.BirthDate);
+            }
+        }
+        public static void DisplayEmployee(string name)
+        {
+            using (StreamReader sr = File.OpenText("SimpleNotebook.txt"))
+            {
+                string temp = null;
+                while ((temp = sr.ReadLine()) != name && temp != null) ;
+                if (temp == name)
+                {
+                    Console.WriteLine("День недели: " + temp);
+                    Console.WriteLine(String.Format("Что нужно сделать: " + sr.ReadLine()));
+                    Console.WriteLine(String.Format("Время события: " + sr.ReadLine()));
+                    Console.WriteLine(String.Format("Дата События: " + sr.ReadLine()));
+                }
+                else
+                    Console.WriteLine("Такой день не найден . .");
+            }
+        }
+    }
+}
